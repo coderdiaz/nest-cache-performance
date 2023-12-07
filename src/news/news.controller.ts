5523874792164809f7a8483db96b7bc323424720
Javier Diaz <coderdiaz@gmail.com>
2023-12-07 16:23:20 -0600

    feat: added support for caching endpoints

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,13 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, UseInterceptors } from '@nestjs/common';
 import { NewsService } from './news.service';
+import { CACHE_MANAGER, CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Controller('/api/news')
 export class NewsController {
-  constructor(private readonly newsService: NewsService) {}
+  constructor(
+    private readonly newsService: NewsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get()
   async news() {
-    const response = await this.newsService.getNews();
-    return response.data;
+    const cachedNews = await this.cacheManager.get('news');
+    if (!cachedNews) {
+      const { data } = await this.newsService.getNews();
+      await this.cacheManager.set('news', data, 0);
+      return data;
+    }
+    return cachedNews;
+  }
+
+  @UseInterceptors(CacheInterceptor)
+  @CacheKey('more')
+  @CacheTTL(0)
+  @Get('/more')
+  async otherNews() {
+    const { data } = await this.newsService.getNews();
+    return data;
   }
 }
