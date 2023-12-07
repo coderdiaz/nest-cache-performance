590844ceec1c53ab63febf18ee0735306d2ac1d2
Javier Diaz <coderdiaz@gmail.com>
2023-12-07 16:23:09 -0600

    feat: configured cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { NewsModule } from './news/news.module';
 import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config]}),
+    CacheModule.register({ isGlobal: true }),
     NewsModule,
   ],
 })
