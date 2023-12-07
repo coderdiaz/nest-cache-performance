2240f9673dab7670d89af1733d8793967a9352fc
Javier Diaz <coderdiaz@gmail.com>
2023-12-07 16:39:48 -0600

    feat: added config variables

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,6 @@ export default () => ({
   port: process.env.PORT || 3000,
   newsApiUrl: process.env.NEWS_API_URL,
   newsApiKey: process.env.NEWS_API_KEY,
+  redisHost: process.env.REDIS_HOST || 'localhost',
+  redisPort: process.env.REDIS_PORT || 6379,
 } as Config);
